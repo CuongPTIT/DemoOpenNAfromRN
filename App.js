6d348a8e5aab9b093d8d271e4918ec1fb34ff7f1
CuongPTIT <thachtuancuong@gmail.com>
2020-03-23 09:54:10 +0700

    add new opensudoku

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import {View, Button, NativeModules, StyleSheet} from 'react-native';
-import EmptyView from './ViewNative';
-import CustomView from './CustomView';
-// import {CustomView} from 'react-native-opensudoku';
+import CustomView from 'react-native-opensudoku';
+// import EmptyView from './ViewNative';
+// import CustomView from './CustomView';
+// import CustomView from 'react-native-opensudoku';
+// import youtube from 'react-native-youtube';
 
 export default class App extends Component {
   render() {
@@ -16,6 +18,7 @@ export default class App extends Component {
         />
         {/* <EmptyView style={style.square} /> */}
         <CustomView style={{height: 200, width: 200}} />
+        <CustomView style={style.square} />
       </View>
     );
   }
